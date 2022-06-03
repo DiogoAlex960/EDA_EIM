f869827aa3c00c7741d7812f374f694016864d54
DiogoAlex960 <diogoalexandre960@gmail.com>
2022-06-03 20:56:54 +0100

    Upload

--- Estrutura.h
+++ Estrutura.h
@@ -2,73 +2,79 @@
 //Diogo Alexandre Mendes Simões Nº15328
 //Estrutura de Dados Avançadas
 
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include<locale.h>
 #include<ctype.h>
-//----------------------INICIO ESTRUTURAS ---------------------
-//----------------------ESTRUTURA MAQUINA ---------------------
+
+
 typedef struct maquina
 {
-    int maquinaNum; //
-    int maquinaTempo;//
-    struct maquina *Seguinte;//
-    struct maquina *Anterior;//
-} maquina;//
-//----------------------ESTRUTURA OPERACAO ---------------------
+    int maquinaNum;
+    int maquinaTempo;
+    struct maquina *Seguinte;
+    struct maquina *Anterior;
+} maquina;
 
-typedef struct operacao//
+typedef struct operacao
 {
-    int operacaoNum;//
-    int jobNum;//
-    struct maquina *MaquinaHead;//
-    struct operacao *Seguinte;//
-    struct operacao *Anterior;//
-} operacao;//
-//----------------------ESTRUTURA JOB ---------------------
+    int operacaoNum;
+    int jobNum;
+    struct maquina *MaquinaHead;
+    struct operacao *Seguinte;
+    struct operacao *Anterior;
+} operacao;
+
 typedef struct job
 {
-    int jobis;//
-    int jobNum;//
-    struct operacao *OperacaoHead;//
-    struct job *Seguinte;//
+    int jobis;
+    int jobNum;
+    struct operacao *OperacaoHead;
+    struct job *Seguinte;
 } job;
-//----------------------FIM ESTRUTURAS---------------------
 
 int Dado();
 
 job *read();
 void write();
 
-//----------------------------Tempos
 void TempoMinimo();
 void TempoMaximo();
 void TempoMedio();
 
-//----------------------------Jobs
 void ListaJobs();
-void CriarJob();
-void RemoverJob();
-job *CriarNovoJob();
-job *HeadJob();
-job *FinalJob();
-job *SelecionaJob();
-
-//----------------------------Operacoes
 void ListaOperacao();
+void ListaMaquina();
+
 void CriarOperacao();
 void RemoverOperacao();
 void AlterarOperacao();
+
+
+
+
+void CriarJob();
+void RemoverJob();
+
+
+job *CriarNovoJob();
 operacao *CriarNovaOperacao();
+maquina *CriarNovaMaquina();
+
+job *HeadJob();
 operacao *CabecalhoOperacao();
+maquina *CabecalhoMaquina();
+
+job *FinalJob();
 operacao *FinalOperacao();
+maquina *FinalMaquina();
+
+job *SelecionaJob();
 operacao *SelecionaOperacao();
+maquina *SelecionaMaquina();
 operacao *ProcuraOperacao();
-//----------------------------Maquinas
-void ListaMaquina();//Listar Maquinas
-maquina *CriarNovaMaquina();//Cria uma nova Maquina
-maquina *CabecalhoMaquina();//Maquina no cabecalho Superior
-maquina *FinalMaquina();//Maquina no cabecalho Inferior
-maquina *SelecionaMaquina();//Selecionar Maquina
-void *RemoveMaquina();//Remover Maquina
+
+void *RemoveMaquina();
